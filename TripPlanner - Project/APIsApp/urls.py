05520a064be59de1f6f0fc557ca76ad427a707c1
Jan Bezler <jan.filip.bezler@gmail.com>
2020-11-13 17:56:14 +0100

    Test button template and url

--- TripPlanner - Project/APIsApp/urls.py
+++ TripPlanner - Project/APIsApp/urls.py
@@ -6,9 +6,9 @@ from APIsApp import views
 router = DefaultRouter()
 router.register(r'routes', views.RouteViewSet, basename="routes")
 
-
 app_name = 'APIsApp'
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('button', views.buttonSiteView, name='buttonSiteView')
 ]
--- TripPlanner - Project/APIsApp/views.py
+++ TripPlanner - Project/APIsApp/views.py
@@ -1,8 +1,21 @@
+from django.shortcuts import render, redirect
 from rest_framework import viewsets
 from APIsApp.serializers import RouteSerializer
 from APIsApp.models import Route
+
+from .routeapi import RouteApiRequest
 
 
 class RouteViewSet(viewsets.ModelViewSet):
     queryset = Route.objects.all()
     serializer_class = RouteSerializer
+
+def buttonSiteView(request):
+    distance = ""
+    coordinates = ""
+    if request.method == "POST":
+        route = RouteApiRequest(17.6865287,53.9324734,17.4627752,52.0239488)
+        distance = route.give_distance()
+        coordinates = route.give_coordinates()
+
+    return render(request, "button.html",{"coordinates":coordinates,"distance":distance})
