a0088b3eaf23fc295cc807c9b37216d5f819b1a8
Jan Bezler <jan.filip.bezler@gmail.com>
2020-11-18 19:37:36 +0100

    Poprawa po code review

--- TripPlanner - Project/APIsApp/views.py
+++ TripPlanner - Project/APIsApp/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render, redirect
 from rest_framework import viewsets
-from APIsApp.serializers import RouteSerializer
-from APIsApp.models import Route
-from APIsApp.forms import CoordinatesForm
+from .serializers import RouteSerializer
+from .models import Route
+from .forms import CoordinatesForm
 from .routeapi import RouteApiRequest
 
 
@@ -10,10 +10,12 @@ class RouteViewSet(viewsets.ModelViewSet):
     queryset = Route.objects.all()
     serializer_class = RouteSerializer
 
-def buttonSiteView(request):
-    
-    if request.method == "POST":
+
+def button_site_view(request):
+
+    if request.method == 'POST':
         form = CoordinatesForm(request.POST)
+
         if form.is_valid():
 
             longitude_start = form.cleaned_data['longitude_start']
@@ -21,12 +23,21 @@ def buttonSiteView(request):
             longitude_end = form.cleaned_data['longitude_end']
             latitude_end = form.cleaned_data['latitude_end']
 
-            route = RouteApiRequest(longitude_start,latitude_start,longitude_end,latitude_end)
+            route = RouteApiRequest(longitude_start,
+                                    latitude_start,
+                                    longitude_end,
+                                    latitude_end)
 
-            route_model = Route(longitude_start=longitude_start,latitude_start=latitude_start,longitude_end=longitude_end,latitude_end=latitude_end,distance=route.give_distance(),coordinates_json=route.give_coordinates())
+            route_model = Route(longitude_start=longitude_start,
+                                latitude_start=latitude_start,
+                                longitude_end=longitude_end,
+                                latitude_end=latitude_end,
+                                distance=route.give_distance(),
+                                coordinates_json=route.give_coordinates())
             route_model.save()
 
-            return redirect("/api/button")
+        return redirect('/api/button')
+
     else:
         form = CoordinatesForm()
-        return render(request, "button.html",{"form":form})
+        return render(request, 'button.html', {'form': form})
