d8e95fb6d0fd1d27d18492605e26f5990d4fd285
Jan Bezler <jan.filip.bezler@gmail.com>
2021-02-14 17:33:52 +0100

    Remove unnecessary imports

--- TripPlanner - Project/APIsApp/views.py
+++ TripPlanner - Project/APIsApp/views.py
@@ -2,8 +2,6 @@ from django.shortcuts import render, redirect
 from rest_framework import viewsets
 from .serializers import RouteSerializer
 from .models import Route
-from .forms import CoordinatesForm
-from .routeapi import RouteApiRequest
 
 
 class RouteViewSet(viewsets.ModelViewSet):
